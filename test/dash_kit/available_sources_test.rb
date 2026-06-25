c5230024b4b2f3a9b877fdaaac7c15540453b15a
tylercschneider <tylercschneider@gmail.com>
2026-06-25 13:08:40 -0400

    Cover host-supplied available_sources callback

--- test/dash_kit/available_sources_test.rb
+++ test/dash_kit/available_sources_test.rb
@@ -10,4 +10,10 @@ class DashKit::AvailableSourcesTest < ActiveSupport::TestCase
   test "available_sources is empty by default" do
     assert_equal [], DashKit.available_sources("viewer-1")
   end
+
+  test "available_sources uses the host callback scoped to the viewer" do
+    DashKit.available_sources_for = ->(viewer) { viewer == "analyst" ? %w[revenue expenses] : [] }
+
+    assert_equal %w[revenue expenses], DashKit.available_sources("analyst")
+  end
 end
